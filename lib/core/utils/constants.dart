1f4145e9f838acb62117daa3946291a000e969f4
UncleTanto <155347986+UncleTanto@users.noreply.github.com>
2024-09-30 12:35:47 +0300

    Update constants.dart

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 const kPrimaryGreen = Color(0xff20473E);
 const kPrimaryYellow = Color(0xffF04C29);
 const kPrimaryOrange = Color(0xffFFCC0A);
-
+const splashscreenimg = "lib/core/assets/scbg.png";
+const logo = "lib/core/assets/logo_shagaf.png";
