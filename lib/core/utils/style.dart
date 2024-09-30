8de6dccca68bd52df6eddf7e3216212c2c8080cc
UncleTanto <155347986+UncleTanto@users.noreply.github.com>
2024-09-30 11:27:30 +0300

    added some comments

--- lib/core/utils/style.dart
+++ lib/core/utils/style.dart
@@ -1,8 +1,13 @@
 
 
+import 'dart:ui';
+
 import 'package:google_fonts/google_fonts.dart';
 
 abstract class Styles{
+  static var comfortaa14 = GoogleFonts.comfortaa(fontSize: 14);
   static var comfortaa16 = GoogleFonts.comfortaa(fontSize: 16);
+  static var comfortaa17 = GoogleFonts.comfortaa(fontSize: 17);
+  static var comfortaa17Medium = GoogleFonts.comfortaa(fontSize: 17,fontWeight: FontWeight.w500);
 
 }
